5401b07754b5c59312ca5fb845122bfabce26114
joshua david jennings <jennings.josh@gmail.com>
2021-09-21 22:31:35 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, names) {
+  return drivers.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === names.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, testString) {
+  return drivers.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(driver, soughtName) {
+  return driver.filter((record) => record.name === soughtName);
+}
